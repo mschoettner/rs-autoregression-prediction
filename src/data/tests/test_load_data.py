3d3a56d3c0b04d0f501e760a94368d6be77d2826
Mikkel Schöttner <mikkel.schoettner@yahoo.de>
2024-03-27 09:54:07 -0400

    Move data loader test script to tests directory

--- src/data/tests/test_load_data.py
+++ src/data/tests/test_load_data.py
@@ -5,6 +5,7 @@ from data.load_data import load_hcp_dset_path, load_data
 
 path_restricted = "/home/mschoett/rs-autoregression-prediction/inputs/behavior/hcp_behavioral_RESTRICTED.csv"
 path_subjects = "/home/mschoett/rs-autoregression-prediction/inputs/subjects/hcp_subjects.csv"
+path_rest_subjects = "/home/mschoett/rs-autoregression-prediction/inputs/subjects/hcp_complete_rest.csv"
 path_h5 = "/home/mschoett/rs-autoregression-prediction/inputs/connectomes/hcp.h5"
 
 
@@ -18,13 +19,13 @@ path_h5 = "/home/mschoett/rs-autoregression-prediction/inputs/connectomes/hcp.h5
 # data_dict = load_hcp_dset_path(path_restricted, path_subjects, test_set=0.1)
 # data_dict = load_hcp_dset_path(path_restricted, path_subjects, val_set=0.1)
 # data_dict = load_hcp_dset_path(path_restricted, path_subjects, atlas_scale=1)
-data_dict = load_hcp_dset_path(path_restricted, path_subjects, n_sample=10, val_set=0.1, test_set=0.1)
+data_dict = load_hcp_dset_path(path_restricted, path_rest_subjects, n_sample=100, val_set=0.15, test_set=0.15)
 
-with h5py.File(path_h5, "r") as h5file:
-    for p in data_dict["train"]:
-        print(h5file[p])
-        # data_list.append(h5file[p][:])
+# with h5py.File(path_h5, "r") as h5file:
+#     for p in data_dict["train"]:
+#         print(h5file[p])
+#         # data_list.append(h5file[p][:])
 
-load_data(path_h5, data_dict['train'])
+data = load_data(path_h5, data_dict['train'])
 
 print("finish")
