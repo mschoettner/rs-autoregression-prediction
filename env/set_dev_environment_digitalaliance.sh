e25c24a21191ae1bd608054f232fc9282a4fdb9b
Mikkel Schöttner <mikkel.schoettner@yahoo.de>
2024-04-09 13:29:18 -0400

    Update environment instructions and fix error in github installation of fmri-autoreg

--- env/set_dev_environment_digitalaliance.sh
+++ env/set_dev_environment_digitalaliance.sh
@@ -1,7 +1,14 @@
-# set up environment on compute canada
-# install on the log in node
-# Install in a virtual environment
-# virtualenv -p /cvmfs/soft.computecanada.ca/easybuild/software/2020/avx512/Core/python/3.10.2/bin/python -a . env_py310
+# Set up environment on compute canada
+# Install on the log in node
+# Run the following commands in a shell before running this script
+# Set to old compute canada module pack:
+# module load StdEnv/2020
+# Load python version:
+# module load python/3.10
+# Install in a virtual environment:
+# virtualenv dev
+# Activate environment:
+# source env/dev/bin/activate
 #!/bin/bash
 
 TORCH=1.13.1
@@ -15,7 +22,7 @@ pip install git+https://github.com/Epistimio/orion.git@v0.2.7
 pip install git+https://github.com/Epistimio/sample-space.git@v1.0.0
 pip install nilearn==0.9.2 hydra-orion-sweeper==1.6.4 hydra-submitit-launcher==1.2.0
 # pip install -e fmri_autoreg
-pip install git+:https://github.com/htwangtw/fmri-autoreg.git:2024.03.2
+pip install git+https://github.com/htwangtw/fmri-autoreg.git@2024.03.2
 pip install -e .
 # style related things below. Feel free to ignore
 module load rust
