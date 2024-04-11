057f2524d14ecf7f60b3f56d7877178d23b461b2
Mikkel Schöttner <mikkel.schoettner@yahoo.de>
2024-04-11 11:54:26 -0400

    Add working train commands

--- src/utils/slurm/submit_training.sh
+++ src/utils/slurm/submit_training.sh
@@ -1,3 +1,47 @@
+# train on 100 HCP subjects with different random seeds
+python src/train.py --multirun  \
+  hydra/launcher=submitit_slurm \
+  ++hydra.launcher.account=rrg-pbellec \
+  ++hydra.launcher.timeout_min=90 \
+  ++hydra.launcher.mem_gb=4 \
+  ++hydra.launcher.gpus_per_node=1 \
+  ++hydra.launcher.cpus_per_task=4 \
+  ++random_state=1,2,3,5,8,13,21,34,55,89
+
+# train one model on all subjects
+python src/train.py --multirun  \
+  hydra/launcher=submitit_slurm \
+  ++data.n_sample=-1 \
+  ++hydra.launcher.account=rrg-pbellec \
+  ++hydra.launcher.timeout_min=90 \
+  ++hydra.launcher.mem_gb=32 \
+  ++hydra.launcher.gpus_per_node=1 \
+  ++hydra.launcher.cpus_per_task=4 \
+
+# scaling over subjects
+python src/train.py --multirun  \
+  hydra/launcher=submitit_slurm \
+  ++data.n_sample=100,200,300,400,500,600,700,900,-1 \
+  ++hydra.launcher.account=rrg-pbellec \
+  ++hydra.launcher.timeout_min=90 \
+  ++hydra.launcher.mem_gb=32 \
+  ++hydra.launcher.gpus_per_node=1 \
+  ++hydra.launcher.cpus_per_task=4 \
+  # ++random_state=1,2,3,5,8,13,21,34,55,89
+
+# higher subject numbers need more time
+python src/train.py --multirun  \
+  hydra/launcher=submitit_slurm \
+  ++data.n_sample=700,900,-1 \
+  ++hydra.launcher.account=rrg-pbellec \
+  ++hydra.launcher.timeout_min=180 \
+  ++hydra.launcher.mem_gb=32 \
+  ++hydra.launcher.gpus_per_node=1 \
+  ++hydra.launcher.cpus_per_task=4
+
+
+
+# Hao-Ting's examples
 # use a small set to make sure the parameter tuning is doing things
 python src/train.py --multirun hydra=hyperparameters ++hydra.launcher.timeout_min=480 ++data.n_sample=-1
 
