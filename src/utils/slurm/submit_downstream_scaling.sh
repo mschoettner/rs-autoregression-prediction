43ad2ac2d10deb9f4078a9268fcc9bc38ed02dc6
Mikkel Schöttner <mikkel.schoettner@yahoo.de>
2024-05-14 13:21:22 -0400

    only one variable at a time

--- src/utils/slurm/submit_downstream_scaling.sh
+++ src/utils/slurm/submit_downstream_scaling.sh
@@ -3,8 +3,8 @@ python src/downstream_scaling.py --multirun \
     ++feature_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/downstream_scaling/extract' \
     ++model_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/downstream_scaling/model'
 
-# predict factor scores and age
+# predict age
 python src/downstream_scaling.py --multirun \
     ++feature_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/downstream_scaling/extract' \
     ++model_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/downstream_scaling/model' \
-    ++predict_variable='mental_health','cognition','processing_speed','substance_use','age'
+    ++predict_variable='age'
