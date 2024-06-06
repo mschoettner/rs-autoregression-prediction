c7a13606f06978ac2c7e5aaa5cf39d2544503435
Mikkel Schöttner <mikkel.schoettner@yahoo.de>
2024-06-06 14:49:22 -0400

    commands to rerun failed and run large model

--- src/utils/slurm/submit_downstream_scaling.sh
+++ src/utils/slurm/submit_downstream_scaling.sh
@@ -3,8 +3,17 @@ python src/downstream_scaling.py --multirun \
     ++feature_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/downstream_scaling/extract' \
     ++model_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/downstream_scaling/model'
 
-# predict age
+# rerun failed job
 python src/downstream_scaling.py --multirun \
     ++feature_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/downstream_scaling/extract' \
     ++model_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/downstream_scaling/model' \
-    ++predict_variable='age'
+    ++predict_variable='age' \
+    ++n_sessions=2 \
+    ++fraction_sample=0.9 \
+    ++hydra.launcher.timeout_min=360
+
+
+# large model
+python src/downstream_scaling.py --multirun \
+    ++feature_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/downstream_scaling/extract_large_2' \
+    ++model_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/downstream_scaling/large_model'
