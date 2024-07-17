3e0709ba256cf1c63dbd0b8869be0f8e752718c6
Mikkel Schöttner <mikkel.schoettner@yahoo.de>
2024-07-17 05:03:40 -0400

    submit commands one pipeline

---
+++ src/utils/slurm/submit_extraction_one_pipe.sh
@@ -0,0 +1,3 @@
+# Minimal example
+python src/extract_one_pipe.py --multirun \
+    ++model_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/train/multiruns/2024-04-29_13-51-25_minimal_example/++data.n_sample\=100\,++data.sessions\=0.25/model.pkl'
