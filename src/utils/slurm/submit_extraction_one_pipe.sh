5a9b60b3a2ba007294485dc00d81a0ea2af7d629
Mikkel Schöttner <mikkel.schoettner@yahoo.de>
2024-09-10 09:08:43 -0400

    one pipe commands

--- src/utils/slurm/submit_extraction_one_pipe.sh
+++ src/utils/slurm/submit_extraction_one_pipe.sh
@@ -1,3 +1,16 @@
 # Minimal example
 python src/extract_one_pipe.py --multirun \
     ++model_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/train/multiruns/2024-04-29_13-51-25_minimal_example/++data.n_sample\=100\,++data.sessions\=0.25/model.pkl'
+
+# Medium model
+python src/extract_one_pipe.py --multirun \
+    ++model_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/train/multiruns/2024-05-28_09-56-51_scaling_medium_rs/++data.n_sample\=-1\,++data.sessions\=4\,++random_state\=1/model.pkl'
+
+# Medium model with several random seeds
+python src/extract_one_pipe.py --multirun \
+    ++random_state=1,2,3,5,8,13,21,34,55,89 \
+    ++model_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/train/multiruns/2024-05-28_09-56-51_scaling_medium_rs/++data.n_sample\=-1\,++data.sessions\=4\,++random_state\=${random_state}/model.pkl'
+
+# Model with best hyperparameters August 13
+python src/extract_one_pipe.py --multirun \
+    ++model_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/train/multiruns/2024-08-13_09-25-10_hyperparameter/best/model.pkl'
--- src/utils/slurm/submit_prediction_one_pipe.sh
+++ src/utils/slurm/submit_prediction_one_pipe.sh
@@ -2,3 +2,8 @@
 python src/predict_one_pipe.py --multirun \
     ++feature_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/minimal_example_one_pipe/extract' \
     ++model_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/minimal_example_one_pipe/model'
+
+# medium model, first random seed
+python src/predict_one_pipe.py --multirun \
+    ++feature_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/medium_model_10_seeds/extract' \
+    ++model_path='/home/mschoett/projects/rrg-pbellec/mschoett/rs-autoregression-prediction/outputs/medium_model_10_seeds/model'
